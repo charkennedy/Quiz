95de671f5b5b038891b0fcbe3d7f3a92ab951f2d
Charlotte Kennedy <ckennedy@daredevil.local>
2019-01-29 16:13:11 -0500

    working P2

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Charlotte Kennedy on 1/23/19.
 //  Copyright © 2019 Charlotte Kennedy. All rights reserved.
+//  High Point University
 //
 
 import UIKit
@@ -16,12 +17,24 @@ class ViewController: UIViewController {
     let questions : [String] = [
         "What is 7+7?",
         "What is the capital of Vermont?",
-        "What is cognac made from?"
+        "What is cognac made from?",
+        "What is the steakhouse called?",
+        "What is 2000-76?",
+        "Who is the president of HPU?",
+        "What building is the pool and basketball court in?",
+        "What is the mascot?"
+        
     ]
     let answers: [String] = [
         "14",
         "Montpelier",
-        "Grapes"
+        "Grapes",
+        "1924 Prime",
+        "1924",
+        "Nido Qubein",
+        "Millis",
+        "Panther"
+        
     ]
     
     var currentQuestionIndex: Int = 0
